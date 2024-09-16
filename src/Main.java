300368d1de33833615ebc6cdea5b80b88345e1cb
sbakhareva <snezhana.bakhareva@yandex.ru>
2024-09-16 12:04:13 +0300

    f

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        // Задача №1
+        int age = 23;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний.");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать.");
+        }
+
+        // Задача №2
 
     }
 }
